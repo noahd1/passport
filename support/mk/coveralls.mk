f0fa6675d107afcb86fb2f28209162afe2a58d57
Noah Davis <noahd1@yahoo.com>
2014-04-08 15:48:31 +0200

    Switch file content

--- support/mk/coveralls.mk
+++ support/mk/coveralls.mk
@@ -1,7 +1,7 @@
-CODECLIMATE ?= ./node_modules/.bin/codeclimate
+COVERALLS ?= coveralls
 
-submit-istanbul-lcov-to-codeclimate:
-	cat $(ISTANBUL_LCOV_INFO_PATH) | $(CODECLIMATE)
+submit-istanbul-lcov-to-coveralls:
+	cat $(ISTANBUL_LCOV_INFO_PATH) | $(COVERALLS)
 
 
-.PHONY: submit-istanbul-lcov-to-codeclimate
+.PHONY: submit-istanbul-lcov-to-coveralls
